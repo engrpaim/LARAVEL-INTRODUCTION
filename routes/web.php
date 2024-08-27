9ae7884af45bf10660f4b1dbf6f23866e693d78e
JB <automation3@smp.local>
2024-08-28 04:50:20 +0800

    ELOQUENT LARAVEL

--- routes/web.php
+++ routes/web.php
@@ -1,44 +1,23 @@
 <?php
 
-use Illuminate\Support\Arr;
+
 use Illuminate\Support\Facades\Route;
+use App\Models\Job;
 
-$jobs = [
 
-                [
-                'id' => 1,
-                'title'=>'Director',
-                'salary'=>'PHP 50,000'
-                ],
-                [
-                'id' => 2,
-                'title'=>'Programmer',
-                    'salary'=>'PHP 20,000'
-                ],
-                [
-                    'id' => 3,
-                    'title'=>'Teacher',
-                    'salary'=>'PHP 30,000'
-                ],
-
-        ];
 
 Route::get('/', function () {
     return view('home');
 });
 
 
-Route::get('/jobs', function () use ($jobs) {
+Route::get('/jobs', function ()  {
     return view('jobs',[
-        'jobs'=> $jobs ]);
+        'jobs'=> Job::all() ]);
 });
 
-Route::get('/jobs/{id}',function ($id) use ($jobs) {
-
-
-        $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
-
-        //dd($job);
+Route::get('/jobs/{id}',function ($id) {
+        $job = Job::find($id);
         return view('job', ['job' => $job]);
 });
 
